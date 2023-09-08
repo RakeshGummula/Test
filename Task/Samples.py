40f4b0cf348e65011a2ab75dcab016293b3c41c0
Raks <Raks.marolix@gmail.com>
2023-09-08 22:22:12 +0530

    My Tasks samples

---
+++ Task/Samples.py
@@ -0,0 +1,34 @@
+# if elif else Example:
+
+# price = int(input("Enter Price:"))
+# if price > 100:
+#     print("price is greater than 100")
+# elif price == 100:
+#     print("price is 100")
+# else :    
+#     print("price is less than 100")
+
+#for loop Example:
+
+# for char in "Rakesh Gummula":
+#     print(char)
+
+#Break Example:
+
+# for i in range(1,9):
+#     if i == 6:
+#         break
+#     print(i)
+
+# Continue Example:
+
+# for i in range(11,20):
+#     if i == 17:
+#         continue
+#     print(i)
+
+#Pass Examples
+
+# for m in [9,7,4]:
+#     pass
+
